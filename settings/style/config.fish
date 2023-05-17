e173944e5589a7a51055b677e766c674f118c555
Darkmux <darkmux.whitehacks@gmail.com>
2023-05-17 04:24:56 -0500

    Fixed some bugs

--- settings/style/config.fish
+++ settings/style/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-chsh -s fish
+cp /data/data/com.termux/files/home/cyberspy/settings/style/config.fish ~/.config/fish
 cat /data/data/com.termux/files/home/cyberspy/settings/style/spy.txt | lolcat
